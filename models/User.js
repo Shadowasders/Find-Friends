1471d82de50a6c34648baba86588d97c55e9a4ee
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-07-27 15:30:02 -0400

    added routes for user and thoughts, finished userController WIP

--- models/User.js
+++ models/User.js
@@ -1,18 +1,47 @@
 //add models for each section of the hw
-const { Schema, Types } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const userSchema = new Schema ({
     username: {
         type: String,
         unique: true,
         required: true,
-        //add trim here(?)
+        trim: true,
     },
 
     email: {
         type: String,
         required: true,
         unique: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "must match an Email"]
+    },
 
-    }
-})
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        }
+    ],
+
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ]
+}, 
+{
+    toJSON: {
+        virtuals: true, 
+
+    },
+    id: false,
+}
+);
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length
+    });
+const User = model("User", userSchema);
+module.exports = User;
